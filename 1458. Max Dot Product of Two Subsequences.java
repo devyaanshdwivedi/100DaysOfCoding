84ecb18feceeb573b329ae08a22dfcc4dcbbeef0
Devyaansh Dwivedi <91078095+devyaanshdwivedi@users.noreply.github.com>
2023-10-14 17:24:21 +0530

    Day_13

--- 1458. Max Dot Product of Two Subsequences.java
+++ 1458. Max Dot Product of Two Subsequences.java
@@ -1,56 +1,52 @@
-class Solution {
-    private int[][] dp = new int[505][505];  // Dynamic programming array
-    private int nums1Size, nums2Size;  // Sizes of nums1 and nums2 arrays
+class Solution:
+    def __init__(self):
+        self.dp = [[-1] * 505 for _ in range(505)]  # Dynamic programming array
+        self.nums1_size = 0
+        self.nums2_size = 0
 
-    // Recursive function to calculate the maximum dot product
-    private int calculateDotProduct(int[] nums1, int[] nums2, int idx1, int idx2) {
-        if (idx1 == nums1Size || idx2 == nums2Size)
-            return 0;
+    # Recursive function to calculate the maximum dot product
+    def calculate_dot_product(self, nums1: list[int], nums2: list[int], idx1: int, idx2: int) -> int:
+        if idx1 == self.nums1_size or idx2 == self.nums2_size:
+            return 0
 
-        if (dp[idx1][idx2] != -1)
-            return dp[idx1][idx2];
+        if self.dp[idx1][idx2] != -1:
+            return self.dp[idx1][idx2]
 
-        // Calculate dot product for three options
-        int option1 = nums1[idx1] * nums2[idx2] + calculateDotProduct(nums1, nums2, idx1 + 1, idx2 + 1);
-        int option2 = calculateDotProduct(nums1, nums2, idx1, idx2 + 1);
-        int option3 = calculateDotProduct(nums1, nums2, idx1 + 1, idx2);
+        # Calculate dot product for three options
+        option1 = nums1[idx1] * nums2[idx2] + self.calculate_dot_product(nums1, nums2, idx1 + 1, idx2 + 1)
+        option2 = self.calculate_dot_product(nums1, nums2, idx1, idx2 + 1)
+        option3 = self.calculate_dot_product(nums1, nums2, idx1 + 1, idx2)
 
-        dp[idx1][idx2] = Math.max(Math.max(option1, option2), option3);  // Store the maximum dot product
-        return dp[idx1][idx2];
-    }
+        self.dp[idx1][idx2] = max(option1, option2, option3)  # Store the maximum dot product
+        return self.dp[idx1][idx2]
 
-    // Main function to calculate the maximum dot product
-    public int maxDotProduct(int[] nums1, int[] nums2) {
-        nums1Size = nums1.length;
-        nums2Size = nums2.length;
 
-        int firstMax = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
-        int firstMin = Integer.MAX_VALUE;
-        int secondMin = Integer.MAX_VALUE;
+    def maxDotProduct(self, nums1: list[int], nums2: list[int]) -> int:
+        self.nums1_size = len(nums1)
+        self.nums2_size = len(nums2)
 
-        // Calculate maximum and minimum values for nums1
-        for (int num : nums1) {
-            firstMax = Math.max(firstMax, num);
-            firstMin = Math.min(firstMin, num);
-        }
-        // Calculate maximum and minimum values for nums2
-        for (int num : nums2) {
-            secondMax = Math.max(secondMax, num);
-            secondMin = Math.min(secondMin, num);
-        }
+        first_max = float('-inf')
+        second_max = float('-inf')
+        first_min = float('inf')
+        second_min = float('inf')
 
-        // Check special cases where all elements are negative
-        if ((firstMax < 0 && secondMin > 0) || (firstMin > 0 && secondMax < 0)) {
-            return Math.max(firstMax * secondMin, firstMin * secondMax);
-        }
+        # Calculate maximum and minimum values for nums1
+        for num in nums1:
+            first_max = max(first_max, num)
+            first_min = min(first_min, num)
+        # Calculate maximum and minimum values for nums2
+        for num in nums2:
+            second_max = max(second_max, num)
+            second_min = min(second_min, num)
 
-        // Initialize dp array with -1
-        for (int i = 0; i < 505; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+        # Check special cases where all elements are negative
+        if (first_max < 0 and second_min > 0) or (first_min > 0 and second_max < 0):
+            return max(first_max * second_min, first_min * second_max)
 
-        // Calculate the maximum dot product recursively
-        return calculateDotProduct(nums1, nums2, 0, 0);
-    }
-}
+        # Initialize dp array with -1
+        for i in range(505):
+            for j in range(505):
+                self.dp[i][j] = -1
+
+        # Calculate the maximum dot product recursively
+        return self.calculate_dot_product(nums1, nums2, 0, 0)
